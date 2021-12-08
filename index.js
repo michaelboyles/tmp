366b77078b94c2dd6ae943b2c455475641937d4f
Michael Boyles <michael@boyl.es>
2021-12-08 22:58:53 +0000

    Add quote component and loads of other crap

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ import { useState } from 'react';
 import { render, screen, act, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
+import { v4 as createId } from 'uuid';
+
+import { Form } from 'informed';
+import createComponentRegistry, {
+  defaultSubQuestions,
+} from '../InternationalRevenue';
+import RenderedQuestionInstance from '../RenderedQuestionInstance';
+
+const QuoteComponent = createComponentRegistry().quoteComponent;
+
+// and lots of other crap
+//gfsag
+//gas
+//gsag asgas
+//gs asgasg
 
 
 function someCode() {
