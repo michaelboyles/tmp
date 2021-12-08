b86a7de01f77a81fede383ab5ff24830dcb5373f
Michael Boyles <michael@boyl.es>
2021-12-08 22:34:09 +0000

    Base js file

---
+++ index.js
@@ -0,0 +1,9 @@
+import { useState } from 'react';
+import { render, screen, act, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import userEvent from '@testing-library/user-event';
+
+
+function someCode() {
+
+}
