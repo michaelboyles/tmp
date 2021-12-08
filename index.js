f79058be7891eb4abba7f500ac9acf7f36c4cde0
Michael Boyles <michael@boyl.es>
2021-12-08 22:36:46 +0000

    Add quote component (develop)

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import { render, screen, act, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 import userEvent from '@testing-library/user-event';
 
+import { Form } from 'informed';
+import createComponentRegistry, {
+  defaultSubQuestions,
+} from '../InternationalRevenue';
+import RenderedQuestionInstance from '../RenderedQuestionInstance';
+
+const QuoteComponent = createComponentRegistry().quoteComponent;
 
 function someCode() {
 
